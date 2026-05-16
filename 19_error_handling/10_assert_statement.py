f62f73d990339339254a30b27a57791374356674
zainsafi <zainulislamk456@gmail.com>
2026-05-16 23:01:51 +0500

    refactor assert statement examples for clarity and consistency

--- 19_error_handling/10_assert_statement.py
+++ 19_error_handling/10_assert_statement.py
@@ -3,21 +3,30 @@
 # if condition is False -> raise AssertionError
 # assert condition, "error message(optional)"
 
-#assert without a message
-#run each program one by one to see the difference
+# run each program one by one to see the difference
+
+# assert without a message
 # print("\nassert without a message")
-# def calculate_square(number):
+# def calculate_square_root(number):
 #     assert number >= 0 # without a message
-#     return number * number
+#     return number ** 0.5
 
-# calculate_square(-2)
+# calculate_square_root(-2)
 
-#assert with a message
-print("\nassert with a message")
-def calculate_square(number):
-    assert number >= 0, "number can't be negative"
-    return number * number
+# #assert with a message
+# print("\nassert with a message")
+# def calculate_square_root(number):
+#     assert number >= 0, "number can't be negative"
+#     return number ** 0.5
 
-print(calculate_square(-2))
+# print(calculate_square_root(-2))
+
 
 # now applying exception handling
+def calulate_square_root(number):
+    assert number >= 0, "number can't be negative"
+    return number ** 0.5
+try:
+    calulate_square_root(-2)
+except AssertionError as e:
+    print(f"Assertion Error: {e}")
